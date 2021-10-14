62e8097042ecf3e958f97dbf11219d038ff76b43
Vasily Krakovetsky <vasis123@gmail.com>
2021-10-14 09:49:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -207,7 +207,7 @@ const farm = {
           $('#accounts').html(JSON.stringify(res.rows[0].balances))
           $('#energy').html('Текущая энергия：' +JSON.stringify(res.rows[0].energy))
           $('#max').html('Максимальная энергия：' +JSON.stringify(res.rows[0].max_energy))
-          if(res.rows[0].energy<200){
+          if(res.rows[0].energy<10){
             _this.recover(parseInt((res.rows[0].max_energy - res.rows[0].energy)/5)*5)
           }
         }
